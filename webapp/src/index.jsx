26d3c56f7e85da3d5458dc21aaacd1c615e5e1e0
殷亮辉 <yinlianghui@hotoa.com>
2020-04-07 13:31:23 +0800

    切换工作区时门户中的store用的是上一个工作区的store
    https://github.com/steedos/creator/issues/1718

--- webapp/src/index.jsx
+++ webapp/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Bootstrap, Dashboard, entityStateSelector, store } from '@steedos/react';
+import { Bootstrap, Dashboard } from '@steedos/react';
 import { getBetweenTimeBuiltinValueItem } from '@steedos/filters';
 
 let config = {
@@ -147,16 +147,15 @@ let config = {
         }
     }
 };
-
-const Home = () => (
-    <Provider store={store}>
-        <Bootstrap>
-            <Dashboard config={config} />
-        </Bootstrap>
-    </Provider>
-)
 class ContractsAppPlugin {
     initialize(registry, store) {
+        const Home = () => (
+            <Provider store={store}>
+                <Bootstrap>
+                    <Dashboard config={config} />
+                </Bootstrap>
+            </Provider>
+        );
         registry.registerObjectHomeComponent(
             'oa_home',
             Home

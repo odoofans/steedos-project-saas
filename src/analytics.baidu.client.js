22e9f54b0c1cbecf2d3c5a581f682fe2649f925c
baozhoutao <baozhoutao@hotoa.com>
2020-04-09 10:05:17 +0800

    实现百度统计
    https://github.com/steedos/creator/issues/1721

---
+++ src/analytics.baidu.client.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2031426ed0c247c3e9a533c0ce347d17";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

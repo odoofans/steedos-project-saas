e5bff7760b7127a26e5dbc0d1614352ad4c3b8e1
baozhoutao <baozhoutao@hotoa.com>
2020-04-26 14:19:43 +0800

    切换路由时，也使用百度统计

--- src/analytics.baidu.client.js
+++ src/analytics.baidu.client.js
@@ -1,7 +1,22 @@
 var _hmt = _hmt || [];
 (function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2031426ed0c247c3e9a533c0ce347d17";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
+  try {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?2031426ed0c247c3e9a533c0ce347d17";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  } catch (error) {
+    console.log(error);
+  }
+
+ try {
+  Meteor.startup(function(){
+    Tracker.autorun(function() {
+      FlowRouter.watchPathChange();
+      window._hmt.push(['_trackPageview', FlowRouter.current().path]);
+    });
+  })
+ } catch (error) {
+  console.log(error);
+ }
 })();
